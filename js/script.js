2e04b3838f51d4657ebf9316f8413675a56de654
christianbyrd <christianmichaelbyrd@gmail.com>
2016-01-03 19:39:06 -0600

    actual rocket

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// This function changes the color of elements with an ID of cool.
+var doCoolStuff = function () {
+	var currentClassName = document.getElementById('cool').className;
+
+	if (currentClassName == 'cool') {
+		document.getElementById('cool').className ='cool seagreen';
+	} else {
+		document.getElementById('cool').className ='cool';
+	}
+}
+
+var sayMyName = function (name) {
+	alert('My name is: '+name);
+}
+
+var car = {
+			make: 'Ford',
+			model: 'Taurus',
+			color: 'blue',
+			isTurnedOn: false,
+			numberOfWheels: 4,
+			seats: ['seat 1', 'seat 2', 'seat 3', 'seat 4'],
+			turnOn: function () {
+				this.isTurnedOn = true;
+			},
+			fly: function () {
+				alert('fly');
+			},
+			switchCar: function (isOn) {
+				console.log('turn car ' +isOn)
+				if (isOn == true) {
+					this.isTurnedOn = true;
+				}
+				else {
+					this.isTurnedOn = false;
+				}
+			}
+		};
